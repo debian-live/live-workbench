0211d75e95d0f72fd7e98710b52558bed228220d
Ben Armstrong <synrg@debian.org>
2009-12-27 10:58:52 -0400

    Initial commit

---
+++ doc/live-workbench.mm
@@ -0,0 +1,102 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1239277914056" ID="Freemind_Link_875705605" MODIFIED="1239279114244" TEXT="Live workbench">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1239278096329" ID="Freemind_Link_66468796" MODIFIED="1239278483331" POSITION="right" TEXT="Rationale">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1239277934764" ID="_" MODIFIED="1239278483368" TEXT="Object-oriented">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1239277948843" ID="Freemind_Link_647169696" MODIFIED="1239278483403" TEXT="Use BDD / RSpec to develop">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1239278102918" ID="Freemind_Link_172375006" MODIFIED="1239278483423" POSITION="right" TEXT="Features">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1239278064711" ID="Freemind_Link_256666938" MODIFIED="1239278483435" TEXT="Manage keys/repos">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1239278156313" ID="Freemind_Link_1258671123" MODIFIED="1239278483453" TEXT="Rapid development">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1239278007402" ID="Freemind_Link_994042077" MODIFIED="1239278483469" TEXT="Support VMs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1239278013211" ID="Freemind_Link_574302293" MODIFIED="1239278483510" TEXT="Caching">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1239278197924" ID="Freemind_Link_260437766" MODIFIED="1239278483530" TEXT="Mocks">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1239279198288" ID="Freemind_Link_1782252759" MODIFIED="1239279200051" TEXT="UI">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1239279213890" ID="Freemind_Link_507863611" MODIFIED="1239279228503" TEXT="colour-highlighted logs">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1239279230766" ID="Freemind_Link_446943124" MODIFIED="1239279233048" TEXT="build.log"/>
+<node COLOR="#111111" CREATED="1239279237572" ID="Freemind_Link_1165194244" MODIFIED="1239279239461" TEXT="live.log"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1239278509689" ID="Freemind_Link_1851836024" MODIFIED="1239278517666" TEXT="Support installer">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1239277989670" ID="Freemind_Link_1469865377" MODIFIED="1239278483536" POSITION="right" TEXT="Early goals">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1239278252278" ID="Freemind_Link_13450612" MODIFIED="1239278483545" TEXT="No GUI">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1239278261540" ID="Freemind_Link_1439273850" MODIFIED="1239278483554" TEXT="Minimal usable wrapper">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1239278297951" ID="Freemind_Link_1579152627" MODIFIED="1239278483565" TEXT="Significant enhancement over plain live-helper">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1239278313374" ID="Freemind_Link_376208141" MODIFIED="1239278483572" TEXT="Transparently support full l-h functionality">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1239278219794" ID="Freemind_Link_942280349" MODIFIED="1239278483579" POSITION="left" TEXT="Background/review">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1239277981645" ID="Freemind_Link_891149423" MODIFIED="1239278483601" TEXT="Compare with live-magic">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1239278678324" ID="Freemind_Link_1908200979" MODIFIED="1239278688162" TEXT="Looks more front-end/user-oriented">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1239278688920" ID="Freemind_Link_732481226" MODIFIED="1239278721287" TEXT="Ease of use a primary goal">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1239278728297" ID="Freemind_Link_1762965448" MODIFIED="1239278731050" TEXT="Python, yuck">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1239278341339" ID="Freemind_Link_657347317" MODIFIED="1239278483608" POSITION="left" TEXT="Later goals">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1239278353341" ID="Freemind_Link_1359552188" MODIFIED="1239278773045" TEXT="Web interface (live-helper-rails)">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1239278358927" ID="Freemind_Link_305738080" MODIFIED="1239278483652" TEXT="Remote">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1239278444009" ID="Freemind_Link_1237390288" MODIFIED="1239278483656" TEXT="Build host">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1239278447056" ID="Freemind_Link_1793372242" MODIFIED="1239278483664" TEXT="Test host">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1239278384150" ID="Freemind_Link_1678395972" MODIFIED="1239278483675" TEXT="Manage images">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1239279009895" ID="Freemind_Link_1723734083" MODIFIED="1239279018072" POSITION="left" TEXT="Purpose">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1239279025152" ID="Freemind_Link_1099043559" MODIFIED="1239279126463" TEXT="Rapid live image development">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1239279071878" ID="Freemind_Link_1248778731" MODIFIED="1239279180000" TEXT="Integrate / automate routine live development tasks">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
