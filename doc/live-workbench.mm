1734d9bddcc5001334b3efe43ddb6a028cec451f
Ben Armstrong <synrg@debian.org>
2010-01-16 19:08:33 -0400

    Clarify developer bias for ruby over python

--- doc/live-workbench.mm
+++ doc/live-workbench.mm
@@ -1,23 +1,29 @@
-<map version="0.8.1">
+<map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node COLOR="#000000" CREATED="1239277914056" ID="Freemind_Link_875705605" MODIFIED="1239279114244" TEXT="Live workbench">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1239278096329" ID="Freemind_Link_66468796" MODIFIED="1239278483331" POSITION="right" TEXT="Rationale">
+<node COLOR="#0033ff" CREATED="1239278096329" ID="Freemind_Link_66468796" MODIFIED="1263682961468" POSITION="right" TEXT="Rationale">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1239277934764" ID="_" MODIFIED="1239278483368" TEXT="Object-oriented">
+<node COLOR="#00b439" CREATED="1239277934764" ID="_" MODIFIED="1263682961469" TEXT="Object-oriented">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1239277948843" ID="Freemind_Link_647169696" MODIFIED="1239278483403" TEXT="Use BDD / RSpec to develop">
+<node COLOR="#00b439" CREATED="1239277948843" ID="Freemind_Link_647169696" MODIFIED="1263682961469" TEXT="Use BDD / RSpec to develop">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1239278102918" ID="Freemind_Link_172375006" MODIFIED="1239278483423" POSITION="right" TEXT="Features">
+<node COLOR="#0033ff" CREATED="1239278102918" ID="Freemind_Link_172375006" MODIFIED="1263682961470" POSITION="right" TEXT="Features">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1239278064711" ID="Freemind_Link_256666938" MODIFIED="1239278483435" TEXT="Manage keys/repos">
+<node COLOR="#00b439" CREATED="1239278064711" ID="Freemind_Link_256666938" MODIFIED="1263682961471" TEXT="Manage keys/repos">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1239278156313" ID="Freemind_Link_1258671123" MODIFIED="1239278483453" TEXT="Rapid development">
+<node COLOR="#00b439" CREATED="1239278156313" ID="Freemind_Link_1258671123" MODIFIED="1263682961472" TEXT="Rapid development">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1239278007402" ID="Freemind_Link_994042077" MODIFIED="1239278483469" TEXT="Support VMs">
 <font NAME="SansSerif" SIZE="14"/>
@@ -29,7 +35,8 @@
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1239279198288" ID="Freemind_Link_1782252759" MODIFIED="1239279200051" TEXT="UI">
+<node COLOR="#00b439" CREATED="1239279198288" ID="Freemind_Link_1782252759" MODIFIED="1263682961474" TEXT="UI">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1239279213890" ID="Freemind_Link_507863611" MODIFIED="1239279228503" TEXT="colour-highlighted logs">
 <font NAME="SansSerif" SIZE="14"/>
@@ -37,28 +44,36 @@
 <node COLOR="#111111" CREATED="1239279237572" ID="Freemind_Link_1165194244" MODIFIED="1239279239461" TEXT="live.log"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1239278509689" ID="Freemind_Link_1851836024" MODIFIED="1239278517666" TEXT="Support installer">
+<node COLOR="#00b439" CREATED="1239278509689" ID="Freemind_Link_1851836024" MODIFIED="1263682961476" TEXT="Support installer">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1239277989670" ID="Freemind_Link_1469865377" MODIFIED="1239278483536" POSITION="right" TEXT="Early goals">
+<node COLOR="#0033ff" CREATED="1239277989670" ID="Freemind_Link_1469865377" MODIFIED="1263682961477" POSITION="right" TEXT="Early goals">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1239278252278" ID="Freemind_Link_13450612" MODIFIED="1239278483545" TEXT="No GUI">
+<node COLOR="#00b439" CREATED="1239278252278" ID="Freemind_Link_13450612" MODIFIED="1263682961477" TEXT="No GUI">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1239278261540" ID="Freemind_Link_1439273850" MODIFIED="1239278483554" TEXT="Minimal usable wrapper">
+<node COLOR="#00b439" CREATED="1239278261540" ID="Freemind_Link_1439273850" MODIFIED="1263682961478" TEXT="Minimal usable wrapper">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1239278297951" ID="Freemind_Link_1579152627" MODIFIED="1239278483565" TEXT="Significant enhancement over plain live-helper">
+<node COLOR="#00b439" CREATED="1239278297951" ID="Freemind_Link_1579152627" MODIFIED="1263682961479" TEXT="Significant enhancement over plain live-helper">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1239278313374" ID="Freemind_Link_376208141" MODIFIED="1239278483572" TEXT="Transparently support full l-h functionality">
+<node COLOR="#00b439" CREATED="1239278313374" ID="Freemind_Link_376208141" MODIFIED="1263682961479" TEXT="Transparently support full l-h functionality">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1239278219794" ID="Freemind_Link_942280349" MODIFIED="1239278483579" POSITION="left" TEXT="Background/review">
+<node COLOR="#0033ff" CREATED="1239278219794" ID="Freemind_Link_942280349" MODIFIED="1263682961480" POSITION="left" TEXT="Background/review">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1239277981645" ID="Freemind_Link_891149423" MODIFIED="1239278483601" TEXT="Compare with live-magic">
+<node COLOR="#00b439" CREATED="1239277981645" ID="Freemind_Link_891149423" MODIFIED="1263682961504" TEXT="Compare with live-magic">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1239278678324" ID="Freemind_Link_1908200979" MODIFIED="1239278688162" TEXT="Looks more front-end/user-oriented">
 <font NAME="SansSerif" SIZE="14"/>
@@ -66,17 +81,20 @@
 <node COLOR="#990000" CREATED="1239278688920" ID="Freemind_Link_732481226" MODIFIED="1239278721287" TEXT="Ease of use a primary goal">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1239278728297" ID="Freemind_Link_1762965448" MODIFIED="1239278731050" TEXT="Python, yuck">
+<node COLOR="#990000" CREATED="1239278728297" ID="Freemind_Link_1762965448" MODIFIED="1263683036544" TEXT="Python is a less-preferred language than Ruby">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1239278341339" ID="Freemind_Link_657347317" MODIFIED="1239278483608" POSITION="left" TEXT="Later goals">
+<node COLOR="#0033ff" CREATED="1239278341339" ID="Freemind_Link_657347317" MODIFIED="1263682961506" POSITION="left" TEXT="Later goals">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1239278353341" ID="Freemind_Link_1359552188" MODIFIED="1239278773045" TEXT="Web interface (live-helper-rails)">
+<node COLOR="#00b439" CREATED="1239278353341" ID="Freemind_Link_1359552188" MODIFIED="1263682961506" TEXT="Web interface (live-helper-rails)">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1239278358927" ID="Freemind_Link_305738080" MODIFIED="1239278483652" TEXT="Remote">
+<node COLOR="#00b439" CREATED="1239278358927" ID="Freemind_Link_305738080" MODIFIED="1263682961507" TEXT="Remote">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1239278444009" ID="Freemind_Link_1237390288" MODIFIED="1239278483656" TEXT="Build host">
 <font NAME="SansSerif" SIZE="14"/>
@@ -85,16 +103,20 @@
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1239278384150" ID="Freemind_Link_1678395972" MODIFIED="1239278483675" TEXT="Manage images">
+<node COLOR="#00b439" CREATED="1239278384150" ID="Freemind_Link_1678395972" MODIFIED="1263682961508" TEXT="Manage images">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1239279009895" ID="Freemind_Link_1723734083" MODIFIED="1239279018072" POSITION="left" TEXT="Purpose">
+<node COLOR="#0033ff" CREATED="1239279009895" ID="Freemind_Link_1723734083" MODIFIED="1263682961513" POSITION="left" TEXT="Purpose">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1239279025152" ID="Freemind_Link_1099043559" MODIFIED="1239279126463" TEXT="Rapid live image development">
+<node COLOR="#00b439" CREATED="1239279025152" ID="Freemind_Link_1099043559" MODIFIED="1263682961514" TEXT="Rapid live image development">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1239279071878" ID="Freemind_Link_1248778731" MODIFIED="1239279180000" TEXT="Integrate / automate routine live development tasks">
+<node COLOR="#00b439" CREATED="1239279071878" ID="Freemind_Link_1248778731" MODIFIED="1263682961515" TEXT="Integrate / automate routine live development tasks">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
