2d7f7427e0ce379736a3efa13247bf84a5fe7c28
Ben Armstrong <synrg@debian.org>
2010-01-19 07:58:08 -0400

    Make init spec pass

--- spec/live-workbench/core_spec.rb
+++ spec/live-workbench/core_spec.rb
@@ -1,14 +1,26 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+require 'tmpdir'
 
 describe LiveWorkbench::Core do
     before :each do
 	@messenger=StringIO.new
 	@lw=LiveWorkbench::Core.new @messenger
+	# FIXME: copied from features/support/hooks.rb
+	@project_dir=Dir.mktmpdir 'live-workbench_project_'
+    end
+
+    after :each do
+	# FIXME: copied from features/support/hooks.rb
+	FileUtils.rm_rf @project_dir
     end
 
     describe '#init' do
+	# FIXME: copied from features/step_definitions.rb
 	it 'should create a directory structure from default' do
-	    pending
+	    Dir.chdir @project_dir
+	    @lw.init
+	    Dir['*','*/**'].sort.should == ["AUTHORS", "COPYING", "Makefile", "README", "TODO",
+    "scripts", "scripts/build", "scripts/clean", "scripts/config"]
 	end
     end
 end
