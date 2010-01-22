01d945b1b05145e85d41fb2ab054d0185e2c6ef6
Ben Armstrong <synrg@debian.org>
2010-01-22 07:27:00 -0400

    Minimal bin wrapper for cli

--- lib/live-workbench/cli.rb
+++ lib/live-workbench/cli.rb
@@ -5,6 +5,17 @@ module LiveWorkbench
 	    @controller=controller
 	end
 
+	def execute args
+	    raise ArgumentError,"No command specified" if args.empty?
+	    command=args.shift.to_sym
+	    raise ArgumentError,"Unrecognized command: #{command}" unless respond_to? command
+	    if args.size>0 
+		send(command,*args)
+	    else
+		send command
+	    end
+	end
+
 	def init template='default'
 	    @controller.init template
 	    @messenger.puts "Project created from #{template} template"
